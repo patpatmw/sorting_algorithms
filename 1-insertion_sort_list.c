fab2ac237d3e1416eb02c5c5d443f9b12a68a9da
patpatmw <patmziya@gmail.com>
2023-01-21 13:40:20 -0800

    sorting

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,13 +1,10 @@
 #include "sort.h"
 
-/**
-
+/*
+ *
  * insertion_sort_list - function that sorts a doubly linked list
-
  * of integers in ascending order using the Insertion sort algorithm
-
  * @list: Dobule linked list to sort
-
  */
 
 void insertion_sort_list(listint_t **list)
@@ -44,16 +41,11 @@ void insertion_sort_list(listint_t **list)
 
 }
 
-/**
-
+/*
  *swap_node - swap a node for his previous one
-
  *@node: node
-
  *@list: node list
-
  *Return: return a pointer to a node which was enter it
-
  */
 
 listint_t *swap_node(listint_t *node, listint_t **list)
